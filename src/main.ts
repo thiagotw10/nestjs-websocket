7e69f5187c4c78ef64f3092877d4f2bec178e84d
thiagotioliveiraa <thiago.oliveira@tisaude.com>
2024-03-31 09:27:00 -0300

    subindo alterações

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './api/app.module';
 import { WebSocketModule } from './websocket/websocket.module';
 
 async function bootstrapApi() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
 
 async function bootstrapWebSocket() {
-  const app = await NestFactory.create(WebSocketModule);
+  const app = await NestFactory.create(WebSocketModule, { cors: true });
   await app.listen(3001);
 }
 
